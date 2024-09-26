09ece0aa275ba61c81f7aca3f07fb4e8f0fa8662
add9010 <101711754+add9010@users.noreply.github.com>
2024-09-26 10:33:59 +0900

    20240926
    Enemy Move

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer SpriteRenderer;
 
     public int nextMove;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-
-        Invoke("Think", 5);
+        anim = GetComponent<Animator>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 2);
     }
 
     // Update is called once per frame
@@ -24,10 +27,8 @@ public class EnemyMove : MonoBehaviour
         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
         if (rayHit.collider == null)
-        {
-            nextMove *= -1;
-            CancelInvoke();
-            Invoke("Think", 5);
+        { 
+            Turn();
         }
     }
 
@@ -35,7 +36,24 @@ public class EnemyMove : MonoBehaviour
     {
         nextMove = Random.Range(-1, 2);
 
-        Invoke("Think",5);
+
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0)
+            SpriteRenderer.flipX = nextMove == 1;
+
+        float nextThinkTime = Random.Range(2f, 4f);
+        Invoke("Think", nextThinkTime);
+
+    }
+
+
+    void Turn()
+    {
+        nextMove *= -1;
+        SpriteRenderer.flipX = nextMove == 1;
+        CancelInvoke();
+        Invoke("Think", 2);
     }
 
 }
